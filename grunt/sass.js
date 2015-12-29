4667e62e246fdd00923893e7b73e2284baefd49f
Arne Hassel <arne.hassel@gmail.com>
2015-12-29 19:48:28 +0100

    Improved CSS a bit for prod

--- grunt/sass.js
+++ grunt/sass.js
@@ -8,5 +8,17 @@ module.exports = {
       'build/css/style.medium.css': 'minimalist/sass/style.medium.scss',
       'build/css/style.large.css': 'minimalist/sass/style.large.scss'
 	  }
+	},
+	'prod': {
+		options: {
+			compass: true,
+			sourcemap: 'none',
+      style: 'compressed'
+		},
+	  files: {
+	    'build/css/style.css': 'minimalist/sass/style.scss',
+      'build/css/style.medium.css': 'minimalist/sass/style.medium.scss',
+      'build/css/style.large.css': 'minimalist/sass/style.large.scss'
+	  }
 	}
 };
