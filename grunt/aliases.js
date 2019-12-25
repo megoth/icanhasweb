ff22b8ca221b8b3f2c394b6e08232f263f7e9822
Arne Hassel <arne.hassel@gmail.com>
2019-12-26 00:34:09 +0100

    A bit of cleaning up

--- grunt/aliases.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  'default': ['dev', 'watch'],
-  'dev': ['wintersmith:local', 'sass:default'],
-  'prod': ['clean:build', 'wintersmith:prod', 'sass:prod', 'webpack:prod']
-}
--- grunt/watch.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	files: ['config.json', 'contents/**/*.md', 'minimalist/sass/**/*.scss', 'templates/*.pug', 'plugins/*'],
-  tasks: ['wintersmith:local', 'sass:default']
-};
